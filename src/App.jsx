b5bee8cc110e853621684334bff908af265534ec
Ifeanyi RG <78403690+waldhari1@users.noreply.github.com>
2024-05-22 08:27:10 +0000

    new update

--- src/App.jsx
+++ src/App.jsx
@@ -24,14 +24,14 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Ifeanyi Onubogu",
+  title: "Software Engineer & Financial Advisor",
+  email: "ifeanyi@waltoria.io",
+  gitHub: "Waldhari12",
+  instagram: "ifeanyi_we",
+  linkedIn: "in/ifeanyi-onubogu",
   medium: "",
-  twitter: "microsoft",
+  twitter: "ifeanyi_we",
   youTube: "Code",
 };
 
